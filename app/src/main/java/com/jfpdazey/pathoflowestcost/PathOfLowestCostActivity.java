effabef7f22b57617fae19cc72bc40a726247f02
John F.P. Dazey <jdazey@pillartechnology.com>
2016-08-08 13:02:08 -0400

    Updated library; clicking on buttons loads grid

--- app/src/main/java/com/jfpdazey/pathoflowestcost/PathOfLowestCostActivity.java
+++ app/src/main/java/com/jfpdazey/pathoflowestcost/PathOfLowestCostActivity.java
@@ -1,15 +1,36 @@
 package com.jfpdazey.pathoflowestcost;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class PathOfLowestCostActivity extends AppCompatActivity {
+
+    public static final Grid EXAMPLE_GRID_1 = new Grid(new int[][]{
+            { 3, 4, 1, 2, 8, 6 },
+            { 6, 1, 8, 2, 7, 4 },
+            { 5, 9, 3, 9, 9, 5 },
+            { 8, 4, 1, 3, 2, 6 },
+            { 3, 7, 2, 8, 6, 4 }
+    });
+
+    public static final Grid EXAMPLE_GRID_2 = new Grid(new int[][]{
+            { 3, 4, 1, 2, 8, 6 },
+            { 6, 1, 8, 2, 7, 4 },
+            { 5, 9, 3, 9, 9, 5 },
+            { 8, 4, 1, 3, 2, 6 },
+            { 3, 7, 2, 1, 2, 3 }
+    });
+
+    public static final Grid EXAMPLE_GRID_3 = new Grid(new int[][]{
+            { 19, 10, 19, 10, 19 },
+            { 21, 23, 20, 19, 12 },
+            { 20, 12, 20, 11, 10 }
+    });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +38,6 @@ public class PathOfLowestCostActivity extends AppCompatActivity {
         setContentView(R.layout.activity_lowest_cost_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
@@ -48,5 +60,28 @@ public class PathOfLowestCostActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void buttonForGridClicked(View view) {
+        String gridContents = getGridContentsForButton(view);
+        ((TextView) findViewById(R.id.grid_contents)).setText(gridContents);
+    }
+
+    private String getGridContentsForButton(View view) {
+        String contents = "";
+
+        switch (view.getId()) {
+            case R.id.grid_1_button:
+                contents = EXAMPLE_GRID_1.asDelimitedString("\t");
+                break;
+            case R.id.grid_2_button:
+                contents = EXAMPLE_GRID_2.asDelimitedString("\t");
+                break;
+            case R.id.grid_3_button:
+                contents = EXAMPLE_GRID_3.asDelimitedString("\t");
+                break;
+        }
+
+        return contents;
     }
 }
--- app/src/test/java/com/jfpdazey/pathoflowestcost/PathOfLowestCostActivityTest.java
+++ app/src/test/java/com/jfpdazey/pathoflowestcost/PathOfLowestCostActivityTest.java
@@ -1,19 +1,47 @@
 package com.jfpdazey.pathoflowestcost;
 
+import android.widget.TextView;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
 @RunWith(RobolectricGradleTestRunner.class)
 @Config(constants = BuildConfig.class, sdk = 21, manifest = "src/main/AndroidManifest.xml", packageName = "com.jfpdazey.pathoflowestcost")
 public class PathOfLowestCostActivityTest {
 
     @Test
-    public void firstMainActivityTest() {
+    public void clickingExampleOneButtonLoadsExampleGridOne() {
         PathOfLowestCostActivity activity = Robolectric.setupActivity(PathOfLowestCostActivity.class);
-        activity.findViewById(R.id.go_button).performClick();
+        String expectedContents = PathOfLowestCostActivity.EXAMPLE_GRID_1.asDelimitedString("\t");
+        activity.findViewById(R.id.grid_1_button).performClick();
+
+        String gridContents = ((TextView) activity.findViewById(R.id.grid_contents)).getText().toString();
+        assertThat(gridContents, equalTo(expectedContents));
     }
 
+    @Test
+    public void clickingExampleTwoButtonLoadsExampleGridTwo() {
+        PathOfLowestCostActivity activity = Robolectric.setupActivity(PathOfLowestCostActivity.class);
+        String expectedContents = PathOfLowestCostActivity.EXAMPLE_GRID_2.asDelimitedString("\t");
+        activity.findViewById(R.id.grid_2_button).performClick();
+
+        String gridContents = ((TextView) activity.findViewById(R.id.grid_contents)).getText().toString();
+        assertThat(gridContents, equalTo(expectedContents));
+    }
+
+    @Test
+    public void clickingExampleThreeButtonLoadsExampleGridThree() {
+        PathOfLowestCostActivity activity = Robolectric.setupActivity(PathOfLowestCostActivity.class);
+        String expectedContents = PathOfLowestCostActivity.EXAMPLE_GRID_3.asDelimitedString("\t");
+        activity.findViewById(R.id.grid_3_button).performClick();
+
+        String gridContents = ((TextView) activity.findViewById(R.id.grid_contents)).getText().toString();
+        assertThat(gridContents, equalTo(expectedContents));
+    }
 }
